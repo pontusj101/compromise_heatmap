6de211d9532aff3d89ac3b4e0092d696a479e8db
pontusj101 <pontusj@kth.se>
2023-11-15 20:04:58 +0000

    First commit

---
+++ agents.py
@@ -0,0 +1,52 @@
+import random
+import gym
+from pyRDDLGym.Core.Policies.Agents import BaseAgent
+from collections import OrderedDict
+
+# Attackers and defenders look the same
+class PassiveCyberAgent(BaseAgent):
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def sample_action(self, state=None):
+        selected_action = next(iter(self.action_space.spaces))
+        action = {selected_action: self.action_space[selected_action]}
+        action[selected_action] = 0
+        print(f'action = {action}')
+        return action
+
+class RandomCyberAgent(BaseAgent):
+    def __init__(self, action_space, seed=None):
+        self.action_space = action_space
+        self.rng = random.Random(seed)
+        if seed is not None:
+            self.action_space.seed(seed)
+
+    def sample_action(self, state=None):
+        s = self.action_space.sample()
+        action = {}
+        selected_action = self.rng.sample(list(s), 1)[0]
+        action[selected_action] = s[selected_action]
+        print(f'RandomCyberAgent action = {action}')
+        return action
+
+class KeyboardCyberAgent(BaseAgent):
+    def __init__(self, action_space, seed=None):
+        self.action_space = action_space
+
+    def sample_action(self, state=None):
+        available_actions = list(self.action_space.spaces.keys())
+
+        print("Available actions:")
+        for i, action in enumerate(available_actions):
+            print(f"{i}. {action}")
+
+        selected_index = int(input("Enter the index of the action you want to take: "))
+
+        if selected_index < 0 or selected_index >= len(available_actions):
+            print("Invalid index. Using a default action.")
+            selected_index = 0
+
+        selected_action = available_actions[selected_index]
+
+        return {selected_action: 1}
---
+++ trainer.py
@@ -0,0 +1,53 @@
+import time
+import numpy
+import random
+from pyRDDLGym import RDDLEnv
+from pyRDDLGym import ExampleManager
+from pyRDDLGym.Visualizer.MovieGenerator import MovieGenerator
+from pyRDDLGym.Core.Policies.Agents import RandomAgent
+from agents import PassiveCyberAgent
+from agents import RandomCyberAgent
+
+base_path = 'content/'
+myEnv = RDDLEnv.RDDLEnv(domain=base_path+'domain.rddl', instance=base_path+'instance.rddl')
+
+start_step =  random.randint(0, 100)
+print(f'Starting attack at step {start_step}')
+agent = PassiveCyberAgent(action_space=myEnv.action_space)
+# agent = RandomCyberAgent(action_space=myEnv.action_space, seed=42)
+
+
+log_trace = []
+total_reward = 0
+state = myEnv.reset()
+start_time = time.time()
+print(f'step         = 0')
+print(f'attack steps = {[attackstep for attackstep, value in state.items() if type(value) is numpy.bool_ and value == True]}')
+print(f'TTCs         = {[(attackstep, value) for attackstep, value in state.items() if type(value) is numpy.int64]}')
+for step in range(myEnv.horizon):
+    if step == start_step:
+      agent = RandomCyberAgent(action_space=myEnv.action_space, seed=42)
+      print(f'Now initiating attack.')
+    action = agent.sample_action()
+    next_state, reward, done, info = myEnv.step(action)
+    observations = [key for key, value in next_state.items() if type(value) is numpy.bool_ and value == True and "observed" in key]
+    if observations:
+      log_trace.append(observations)
+    attacksteps = [key for key, value in next_state.items() if type(value) is numpy.bool_ and value == True and "observed" not in key]
+    total_reward += reward
+    print()
+    print(f'step              = {step}')
+    print(f'action            = {action}')
+    print(f'observations      = {observations}')
+    print(f'log trace         = {log_trace}')
+    print(f'attack steps      = {attacksteps}')
+    print(f'TTCs              = {[(attackstep, value) for attackstep, value in next_state.items() if type(value) is numpy.int64]}')
+    print(f'reward            = {reward}')
+    state = next_state
+    if done:
+        break
+end_time = time.time()
+print()
+print(f'episode ended with reward {total_reward}. Execution time was {end_time-start_time} s.')
+
+myEnv.close()
