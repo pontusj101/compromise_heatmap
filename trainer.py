ca4adb842656621295a75bce19823db24b5cfe0e
pontusj101 <pontusj@kth.se>
2023-11-16 17:43:05 +0000

    Log starts with non-malicious events.

--- trainer.py
+++ trainer.py
@@ -47,7 +47,7 @@ data_series = []
 base_path = 'content/'
 myEnv = RDDLEnv.RDDLEnv(domain=base_path+'domain.rddl', instance=base_path+'instance.rddl')
 
-start_step =  random.randint(10, 100)
+start_step =  random.randint(time_steps, 100)
 print(f'Starting attack at step {start_step}')
 agent = PassiveCyberAgent(action_space=myEnv.action_space)
 # agent = RandomCyberAgent(action_space=myEnv.action_space, seed=42)
@@ -62,8 +62,8 @@ print(f'attack steps = {[attackstep for attackstep, value in state.items() if ty
 print(f'TTCs         = {[(attackstep, value) for attackstep, value in state.items() if type(value) is numpy.int64]}')
 for step in range(myEnv.horizon):
     if step == start_step:
-      agent = RandomCyberAgent(action_space=myEnv.action_space, seed=42)
-      print(f'Now initiating attack.')
+        agent = RandomCyberAgent(action_space=myEnv.action_space, seed=42)
+        print(f'Now initiating attack.')
     action = agent.sample_action()
     next_state, reward, done, info = myEnv.step(action)
     observations = [key for key, value in next_state.items() if type(value) is numpy.bool_ and value == True and "observed" in key]
@@ -82,28 +82,31 @@ for step in range(myEnv.horizon):
     print(f'reward            = {reward}')
     state = next_state
 
-    # Initialize feature vectors with -1
-    log_feature_vectors = torch.full((N, time_steps), -1)
+    if step >= time_steps:
+        print(f'Now initiating logging (having populated the log with non-malicious data).')
 
-    # Update feature vectors based on log traces
-    for t, log_t in enumerate(truncated_log_trace):
-        for node in range(N):
-            # Check if node is observable at this time step
-            if any(log_event in log_t for log_event, idx in node_mapping.items() if idx == node):
-                log_feature_vectors[node][t] = 1
-            elif log_feature_vectors[node][t] == -1:
-                log_feature_vectors[node][t] = 0
+        # Initialize feature vectors with -1
+        log_feature_vectors = torch.full((N, time_steps), -1)
 
-    combined_features = torch.cat((node_features, log_feature_vectors), dim=1)
+        # Update feature vectors based on log traces
+        for t, log_t in enumerate(truncated_log_trace):
+            for node in range(N):
+                # Check if node is observable at this time step
+                if any(log_event in log_t for log_event, idx in node_mapping.items() if idx == node):
+                    log_feature_vectors[node][t] = 1
+                elif log_feature_vectors[node][t] == -1:
+                    log_feature_vectors[node][t] = 0
 
-    # Create PyTorch Geometric data object
-    data = Data(x=combined_features, edge_index=edge_index)
+        combined_features = torch.cat((node_features, log_feature_vectors), dim=1)
 
-    # Print the conttent of the data object
-    print(data.x)
-    print(data.edge_index)
+        # Create PyTorch Geometric data object
+        data = Data(x=combined_features, edge_index=edge_index)
 
-    data_series.append(data)
+        # Print the conttent of the data object
+        print(data.x)
+        print(data.edge_index)
+
+        data_series.append(data)
 
     if done:
         break
