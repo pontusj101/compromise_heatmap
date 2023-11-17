825db45cc7b5a0284a078779744fc38403e592e5
pontusj101 <pontusj@kth.se>
2023-11-17 14:27:34 +0000

    Refactored by ChatGPT.

--- gnn_trainer.py
+++ gnn_trainer.py
@@ -4,8 +4,8 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from sklearn.metrics import precision_score, recall_score, f1_score
 from torch_geometric.loader import DataLoader
+from sklearn.metrics import precision_score, recall_score, f1_score
 from data_producer import produce_training_data
 
 class GCN(torch.nn.Module):
@@ -22,20 +22,10 @@ class GCN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=1)
 
-
-def train(use_saved_data=True, n_simulations=10, log_window = 20, max_start_time_step=30, number_of_epochs=10, debug_print=False):
-    
-    data_series = produce_training_data(use_saved_data=use_saved_data, n_simulations=n_simulations, 
-                                        log_window=log_window, 
-                                        max_start_time_step=max_start_time_step, 
-                                        rddl_path='content/', 
-                                        random_cyber_agent_seed=42, 
-                                        debug_print=debug_print)
-
-    # Creating masks
+def create_masks(data_series):
     for data in data_series:
         num_nodes = data.num_nodes
-        all_indices = torch.randperm(num_nodes)  # Shuffling indices using PyTorch
+        all_indices = torch.randperm(num_nodes)
 
         train_size = int(0.7 * num_nodes)
         val_size = int(0.15 * num_nodes)
@@ -52,75 +42,27 @@ def train(use_saved_data=True, n_simulations=10, log_window = 20, max_start_time
         data.val_mask[val_indices] = True
         data.test_mask[test_indices] = True
 
-
-
-    # Initialize model
-    model = GCN(num_node_features=log_window+1, num_classes=2)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-    # DataLoader
-    data_loader = DataLoader(data_series, batch_size=1, shuffle=True)
-
-    # Evaluation function
-    def evaluate_model(model, data_loader, mask):
-        model.eval()
-        total_loss = 0
-        all_predicted_labels = []
-        all_true_labels = []
-        with torch.no_grad():
-            for batch in data_loader:
-                out = model(batch)
-                loss = F.nll_loss(out[mask], batch.y[mask])
-                total_loss += loss.item()
-
-                # Predictions
-                predicted_labels = out[mask].max(1)[1]
-                all_predicted_labels.append(predicted_labels.cpu().numpy())
-
-                # True labels
-                true_labels = batch.y[mask]
-                all_true_labels.append(true_labels.cpu().numpy())
-
-        all_predicted_labels = np.concatenate(all_predicted_labels)
-        all_true_labels = np.concatenate(all_true_labels)
-
-        return total_loss / len(data_loader), all_predicted_labels, all_true_labels
-
-    # Training loop with validation
-    loss_values, val_loss_values = [], []
-    precision_values, recall_values, f1_values = [], [], []
-    for epoch in range(number_of_epochs):  # number of epochs
-        start_time = time.time()
-        model.train()
-        epoch_loss = 0.0
+def evaluate_model(model, data_loader, mask):
+    model.eval()
+    total_loss = 0
+    all_predicted_labels = []
+    all_true_labels = []
+    with torch.no_grad():
         for batch in data_loader:
-            optimizer.zero_grad()
             out = model(batch)
-            loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.item()
-        epoch_loss /= len(data_loader)
-        loss_values.append(epoch_loss)
+            loss = F.nll_loss(out[mask], batch.y[mask])
+            total_loss += loss.item()
+            predicted_labels = out[mask].max(1)[1]
+            all_predicted_labels.append(predicted_labels.cpu().numpy())
+            true_labels = batch.y[mask]
+            all_true_labels.append(true_labels.cpu().numpy())
 
-    # Validation
-        val_loss, predicted_labels, true_labels = evaluate_model(model, data_loader, batch.val_mask)
-        val_loss_values.append(val_loss)
-        end_time = time.time()
-        print(f'Epoch {epoch}: Training Loss: {epoch_loss:.4f}, Validation Loss: {val_loss:.4f}. Time: {end_time - start_time:.4f}s')
+    all_predicted_labels = np.concatenate(all_predicted_labels)
+    all_true_labels = np.concatenate(all_true_labels)
 
-        precision = precision_score(true_labels, predicted_labels, average='binary')
-        recall = recall_score(true_labels, predicted_labels, average='binary')
-        f1 = f1_score(true_labels, predicted_labels, average='binary')
+    return total_loss / len(data_loader), all_predicted_labels, all_true_labels
 
-        precision_values.append(precision)
-        recall_values.append(recall)
-        f1_values.append(f1)
-
-        print(f'Epoch {epoch}: Precision: {precision}, Recall: {recall}, F1 Score: {f1}')
-
-
-    # Plotting
+def plot_training_results(loss_values, val_loss_values):
     plt.figure(figsize=(10, 6))
     plt.plot(loss_values, label='Training Loss')
     plt.plot(val_loss_values, label='Validation Loss')
@@ -131,13 +73,47 @@ def train(use_saved_data=True, n_simulations=10, log_window = 20, max_start_time
     plt.savefig('loss_curve.png')
     plt.close()
 
-    # Test evaluation (optional if you want to see test performance)
+def train_model(use_saved_data=True, n_simulations=10, log_window=20, max_start_time_step=30, number_of_epochs=10, debug_print=False):
+    data_series = produce_training_data(use_saved_data, n_simulations, log_window, max_start_time_step, 'content/', 42, debug_print)
+    create_masks(data_series)
+
+    first_graph = data_series[0]
+    actual_num_features = first_graph.num_node_features
+
+    model = GCN(num_node_features=actual_num_features, num_classes=2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    data_loader = DataLoader(data_series, batch_size=1, shuffle=True)
+
+    loss_values, val_loss_values = [], []
+    for epoch in range(number_of_epochs):
+        start_time = time.time()
+        model.train()
+        epoch_loss = 0.0
+        for batch in data_loader:
+            optimizer.zero_grad()
+            out = model(batch)
+            loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+        epoch_loss /= len(data_loader)
+        loss_values.append(epoch_loss)
+
+        val_loss, predicted_labels, true_labels = evaluate_model(model, data_loader, batch.val_mask)
+        val_loss_values.append(val_loss)
+        end_time = time.time()
+        print(f'Epoch {epoch}: Training Loss: {epoch_loss:.4f}, Validation Loss: {val_loss:.4f}. Time: {end_time - start_time:.4f}s')
+
+        precision = precision_score(true_labels, predicted_labels, average='binary')
+        recall = recall_score(true_labels, predicted_labels, average='binary')
+        f1 = f1_score(true_labels, predicted_labels, average='binary')
+        print(f'Epoch {epoch}: Precision: {precision}, Recall: {recall}, F1 Score: {f1}')
+
+    plot_training_results(loss_values, val_loss_values)
+
     test_loss, predicted_labels, true_labels = evaluate_model(model, data_loader, batch.test_mask)
-    precision = precision_score(true_labels, predicted_labels, average='binary')
-    recall = recall_score(true_labels, predicted_labels, average='binary')
-    f1 = f1_score(true_labels, predicted_labels, average='binary')
     print(f'Test Loss: {test_loss:.4f}')
     print(f'Test: Precision: {precision}, Recall: {recall}, F1 Score: {f1}')
 
-
-train(use_saved_data=True, n_simulations=10, log_window = 200, max_start_time_step=300, debug_print=False)
+train_model()
