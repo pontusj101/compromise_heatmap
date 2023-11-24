78bfe93a42627282e106ab5f118aa172d1df84d8
pontusj101 <pontusj@kth.se>
2023-11-24 12:58:04 +0000

    Before parameterizing GNN.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for n_simulations in [8]:
             use_saved_data=False, 
             n_simulations=n_simulations, 
             log_window=log_window, 
-            game_time= 70, 
+            game_time= 500, 
             max_start_time_step=40, 
             graph_size='small', 
             random_cyber_agent_seed=0, 
