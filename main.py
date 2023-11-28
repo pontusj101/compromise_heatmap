adaf9a94c0bb0cff281e8142e2bb48b9e353350e
pontusj101 <pontusj@kth.se>
2023-11-28 06:52:30 +0000

    Command line arguments.

--- main.py
+++ main.py
@@ -1,70 +1,85 @@
+import argparse
 import logging
 from animator import create_animation
 from instance_creator import create_instance
 from simulator import produce_training_data_parallel
 from gnn_trainer import train_gnn
 
+# Initialize parser
+parser = argparse.ArgumentParser(description='Run different modes of the security simulation program.')
+
+# Adding arguments
+parser.add_argument('mode', choices=['create_instance', 'produce_training_data', 'train_gnn', 'animate', 'evaluate'], help='Mode of operation')
+parser.add_argument('--instance_type', default='static', choices=['static', 'dynamic'], help='Type of instance to create')
+parser.add_argument('--size', default='small', choices=['small', 'medium', 'large'], help='Size of the graph')
+parser.add_argument('--game_time', type=int, default=70, help='Time horizon for the simulation')
+parser.add_argument('--n_simulations', type=int, default=128, help='Number of simulations to run')
+parser.add_argument('--log_window', type=int, default=3, help='Size of the logging window')
+parser.add_argument('--random_cyber_agent_seed', default=None, help='Seed for random cyber agent')
+parser.add_argument('--number_of_epochs', type=int, default=8, help='Number of epochs for GNN training')
+parser.add_argument('--learning_rate', type=float, default=0.005, help='Learning rate for GNN training')
+parser.add_argument('--batch_size', type=int, default=256, help='Batch size for GNN training')
+parser.add_argument('--hidden_layers', nargs='+', type=int, default=[64, 64], help='Hidden layers configuration for GNN')
+parser.add_argument('--rddl_path', default='content/', help='Path to the RDDL files')
+parser.add_argument('--tmp_path', default='tmp/', help='Temporary file path')
+parser.add_argument('--snapshot_sequence_path', default='snapshot_sequences/', help='Path to snapshot sequences')
+parser.add_argument('--training_sequence_file_name', default='snapshot_sequences/latest20231127_192822.pkl', help='Filename for training sequence')
+parser.add_argument('--animation_sequence_filename', default='snapshot_sequences/latest20231127_191906.pkl', help='Filename for animation sequence')
+parser.add_argument('--animation_predictor_filename', default='models/model_hl_[64, 64]_n_6287_lr_0.005_bs_256.pt', help='Filename for the animation predictor model')
+parser.add_argument('--animation_predictor_type', default='gnn', choices=['gnn', 'other'], help='Type of animation predictor')
+
+# Parse arguments
+args = parser.parse_args()
+
+# Set up logging
 logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(asctime)s - %(message)s')
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
 logging.warning('\n\n')
 
-mode = 'animate' # 'create_instance', 'produce_training_data', 'train_gnn', 'animate', 'evaulate'
-instance_type = 'static'
-size = 'small'
-game_time=70 # horizon = 70 good for the small graph, 500 good for the medium-sized graph, 1000 good for the large graph
-rddl_path='content/'
-tmp_path='tmp/'
-snapshot_sequence_path = 'snapshot_sequences/'
-training_sequence_file_name = 'snapshot_sequences/latest20231127_192822.pkl'
-animation_sequence_filename = 'snapshot_sequences/latest20231127_191906.pkl'
-animation_predictor_filename='models/model_hl_[64, 64]_n_6287_lr_0.005_bs_256.pt'
-n_simulations = 128
-log_window = 3
-random_cyber_agent_seed=None
-number_of_epochs = 8
-learning_rate=0.005
-batch_size = 256
-hidden_layers=[64, 64]
-animation_predictor_type='gnn'
+max_start_time_step = args.log_window + int((args.game_time - args.log_window) / 2)
+max_log_steps_after_total_compromise = int(args.log_window / 2)
 
-max_start_time_step = log_window + int((game_time-log_window)/2)
-max_log_steps_after_total_compromise = int(log_window/2)
-
-if mode == 'create_instance':
+# Modes
+if args.mode == 'create_instance':
     # TODO: Write graph_index to file 
     logging.info(f'Creating new instance specification.')
-    rddl_file_path, graph_index_file_path = create_instance(instance_type=instance_type, size=size, horizon=game_time, rddl_path=rddl_path)
-    logging.info(f'Instance specification written to {rddl_file_path}. Graph index written to {graph_index_file_path}.')
+    rddl_file_path, graph_index_file_path = create_instance(instance_type=args.instance_type, size=args.size, horizon=args.game_time, rddl_path=args.rddl_path)
+    s = f'Instance specification written to {rddl_file_path}. Graph index written to {graph_index_file_path}.'
+    logging.info(s)
+    print(s)
 
-if mode == 'produce_training_data':
+elif args.mode == 'produce_training_data':
     logging.info(f'Producing training data.')
     sequence_file_name = produce_training_data_parallel(
-        n_simulations=n_simulations, 
-        log_window=log_window, 
+        n_simulations=args.n_simulations, 
+        log_window=args.log_window, 
         max_start_time_step=max_start_time_step, 
         max_log_steps_after_total_compromise=max_log_steps_after_total_compromise,
-        rddl_path=rddl_path, 
-        tmp_path=tmp_path,
-        snapshot_sequence_path=snapshot_sequence_path,
-        random_cyber_agent_seed=random_cyber_agent_seed)
-    logging.info(f'Training data produced and written to {sequence_file_name}.')
+        rddl_path=args.rddl_path, 
+        tmp_path=args.tmp_path,
+        snapshot_sequence_path=args.snapshot_sequence_path,
+        random_cyber_agent_seed=args.random_cyber_agent_seed)
+    s = f'Training data produced and written to {sequence_file_name}.'
+    logging.info(s)
+    print(s)
 
-if mode == 'train_gnn':
+elif args.mode == 'train_gnn':
     logging.info(f'Training GNN.')
     test_true_labels, test_predicted_labels, predictor_filename = train_gnn(
-                    number_of_epochs=number_of_epochs, 
-                    sequence_file_name=training_sequence_file_name, 
-                    learning_rate=learning_rate, 
-                    batch_size=batch_size, 
-                    hidden_layers=hidden_layers)
-    logging.info(f'GNN trained. Model written to {predictor_filename}.')
+                    number_of_epochs=args.number_of_epochs, 
+                    sequence_file_name=args.training_sequence_file_name, 
+                    learning_rate=args.learning_rate, 
+                    batch_size=args.batch_size, 
+                    hidden_layers=args.hidden_layers)
+    s = f'GNN trained. Model written to {predictor_filename}.'
+    logging.info(s)
+    print(s)
 
-if mode == 'animate':
+elif args.mode == 'animate':
     logging.info(f'Creating animation.')
-    create_animation(animation_sequence_filename=animation_sequence_filename, 
-                     predictor_type=animation_predictor_type, 
-                     predictor_filename = animation_predictor_filename)
-    logging.info(f'Animation created.')
-
-if 'evaulate' in mode:
-    pass
+    create_animation(animation_sequence_filename=args.animation_sequence_filename, 
+                     predictor_type=args.animation_predictor_type, 
+                     predictor_filename=args.animation_predictor_filename)
+    s = f'Animation written to file network_animation.gif.'
+    logging.info(s)
+    print(s)
