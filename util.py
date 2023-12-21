948e6b38bc1320e227febb57436b70a717ea87ed
pontusj101 <pontusj@kth.se>
2023-12-21 06:59:59 +0000

    Debugging.

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def get_sequence_filenames(bucket_manager, sequence_dir_path, min_nodes, max_nod
     log_window_filtered_filenames = [blob.name for blob in bucket_manager.bucket.list_blobs(prefix=prefix)]
     node_num_filtered_filenames = [fn for fn in log_window_filtered_filenames if int(fn.split('/')[2].split('_')[0]) >= min_nodes and int(fn.split('/')[2].split('_')[0]) <= max_nodes]
     random.shuffle(node_num_filtered_filenames)
-    if max_sequences + n_validation_sequences > len(node_num_filtered_filenames):
-        raise ValueError(f'Not enough sequences for training and validation. {len(node_num_filtered_filenames)} sequences found, but {max_sequences} (training) + {n_validation_sequences} (validation) requested.')
-    return node_num_filtered_filenames[:max_sequences], node_num_filtered_filenames[max_sequences:max_sequences+n_validation_sequences] # Limit the number of sequences to max_sequences
+    if len(node_num_filtered_filenames) < n_validation_sequences + 1:
+        raise ValueError(f'Not enough sequences for training and validation. {len(node_num_filtered_filenames)} sequences found, but {n_validation_sequences} validation sequences requested, and at least one additional is required for training.')
+    return node_num_filtered_filenames[:n_validation_sequences], node_num_filtered_filenames[n_validation_sequences:n_validation_sequences+max_sequences:] # Limit the number of sequences to max_sequences
 
