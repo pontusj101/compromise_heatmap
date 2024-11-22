6ad84bd6d2d761bb380ce241259d902b34aec647
Nikolaos Kakouros <nkakouros@google.com>
2024-11-22 12:21:02 +0000

    Support multiple agents in sim.sh

--- sim.sh
+++ sim.sh
@@ -2,24 +2,48 @@
 
 set -euxo pipefail
 
-./heatmap simulate --attacker bfs-random --horizon 1000
-./heatmap simulate --attacker dfs-random --horizon 1000
+HORIZON=1000
 
-python <<PYTHON
-import json,os
-with open('DepthFirstAttacker-logs.json') as f:
-  logs = json.load(f)
+generate_attacker_logs() {
+  ./heatmap simulate --attacker bfs-random --horizon $HORIZON
+
+  python <<PYTHON
+import json
+
 with open('BreadthFirstAttacker-logs.json') as f:
-  logs.extend(json.load(f))
+  logs = json.load(f)
 
 logs.sort(key=lambda l: l['timestamp'])
 
-old_logs = []
-if os.path.isfile("merged-logs.json"):
-  with open("merged-logs.json", "r") as f:
-    old_logs = json.load(f)
-
 with open("merged-logs.json", "w") as f:
-  old_logs.extend(logs)
   json.dump(logs, f, indent=2)
 PYTHON
+}
+
+generate_user_logs() {
+  ./heatmap simulate --attacker dfs-random --horizon $HORIZON
+
+  python <<PYTHON
+import json
+import os
+
+with open('DepthFirstAttacker-logs.json') as f:
+  logs = json.load(f)
+
+old_logs = []
+with open("merged-logs.json", "r") as f:
+  old_logs = json.load(f)
+
+old_logs.extend(logs)
+old_logs.sort(key=lambda l: l['timestamp'])
+
+with open("merged-logs.json", "w") as f:
+  json.dump(old_logs, f, indent=2)
+PYTHON
+}
+
+generate_attacker_logs
+generate_user_logs
+generate_user_logs
+generate_user_logs
+generate_user_logs
