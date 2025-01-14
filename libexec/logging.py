668191efa30fcfff6b56206985eb5bbeb99dd957
Nikolaos Kakouros <nkakouros@google.com>
2025-01-14 00:48:33 +0000

    Remove obsolete stuff from logging module

--- libexec/logging.py
+++ libexec/logging.py
@@ -5,22 +5,6 @@ debug = False
 
 
 def setup_logging(name=None):
-    warnings.filterwarnings(
-        "ignore",
-        message="Tight layout not applied. tight_layout cannot make axes height small enough to accommodate all axes decorations",
-        module="pyRDDLGym.Visualizer.ChartViz",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        message="Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all axes decorations.",
-        module="pyRDDLGym.Visualizer.ChartViz",
-    )
-    warnings.filterwarnings(
-        "ignore",
-        message="Attempting to set identical low and high ylims makes transformation singular; automatically expanding.",
-        module="pyRDDLGym.Visualizer.ChartViz",
-    )
-
     name = name or __package__
 
     root_logger = logging.getLogger()
@@ -40,9 +24,9 @@ def setup_logging(name=None):
         ],
     )
 
-    # TODO: keep only one, the correct
     logging.getLogger("malsim.sims.mal_simulator").setLevel(logging.WARNING)
-    logging.getLogger("malsim").setLevel(logging.WARNING)
+    # logging.getLogger("malsim").setLevel(logging.WARNING)
+    # logging.getLogger("maltoolbox.language.languagegraph").setLevel(logging.DEBUG)
     for logger_name in [name]:
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.DEBUG if debug else logging.INFO)
